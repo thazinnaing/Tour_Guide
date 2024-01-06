c45ef06df76a64269195deeeb4ee8dad059d1d06
thazinnaing <thazinaingmm@gmail.com>
2024-01-06 14:09:04 +0630

    feat: added custom box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "primary": "#24AAF7",
-        "secondary": "#FF9157"
-      }
+        primary: "#24AAF7",
+        secondary: "#FF9157",
+      },
+      boxShadow: {
+        custom: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+      },
     },
   },
   plugins: [],
-}
+};
