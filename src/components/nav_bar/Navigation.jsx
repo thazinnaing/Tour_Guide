17494ac2c43a67cb92652a12f32d0fe2ca270f61
Thazin <thazinnaingmm@gmail.com>
2024-01-08 21:25:34 +0630

    feat: add hamburger tab

--- src/components/nav_bar/Navigation.jsx
+++ src/components/nav_bar/Navigation.jsx
@@ -1,20 +1,29 @@
 import { cn } from "../../utils";
 import Tab from "../Tab";
-import { Button, Div } from "../base";
+import { Button, Div, H3 } from "../base";
 import { navTabs } from "../Data";
+import { AlignJustify, PlaneTakeoff } from "lucide-react";
 
-const Navigation = ({className, ...rest}) => {
+const Navigation = () => {
   return (
-    <Div className={cn('flex justify-between', className)} {...rest}>
-      <Div>Logo</Div>
-      <Div className={cn('flex justify-between gap-10')}>
+    <Div className={cn('flex justify-between items-center')}>
+      <Div className={cn('flex gap-1 items-center')}>
+        <PlaneTakeoff size={30} className={cn('text-secondary')}/>
+        <H3>Touro</H3>
+      </Div>
+      <Div className={cn('flex justify-between hidden sm:flex sm:gap-5 md:gap-10')}>
         {navTabs.map(tab => {
           return (
             <Tab key={tab.id} data={tab}/>
           )
         })}
       </Div>
-      <Button className={cn('bg-secondary')}>Login</Button>
+      <Div className={cn('flex gap-10')}>
+        <Button className={cn('bg-secondary px-5 py-2')}>Login</Button>
+        <Div className={cn('flex items-center sm:hidden')}>
+          <AlignJustify size={30}/>
+        </Div>
+      </Div> 
     </Div>
   )
 }
