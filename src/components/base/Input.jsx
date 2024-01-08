ccca65c4cfa701568eeaef7d6d2b9bda26c8c8c1
Thazin <thazinnaingmm@gmail.com>
2024-01-09 00:16:20 +0630

    style: style signup component

--- src/components/base/Input.jsx
+++ src/components/base/Input.jsx
@@ -2,7 +2,7 @@ import { cn } from "../../utils"
 
 export const Input = ({className, ...rest}) => {
   return (
-    <input className={cn('px-2', className)} type="text" {...rest}/>
+    <input className={cn('px-2 outline-none sm:text-l', className)} type="text" {...rest}/>
   )
 }
 
