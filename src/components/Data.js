1403511358744bbe003ca4cf11c7e3f1892a7310
Thazin <thazinnaingmm@gmail.com>
2024-01-08 20:46:31 +0630

    fix: fix importing

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,3 +5,10 @@ export const tabs = [
   {id: 3, title: "Icebergs"},
   {id: 'view_all', title: "View all"}
 ]
+
+export const navTabs = [
+  {id: 0, title: "Home"},
+  {id: 1, title: "Category"},
+  {id: 2, title: "Blog"},
+  {id: 5, title: "About Us"}
+]
--- src/components/nav_bar/Navigation.jsx
+++ src/components/nav_bar/Navigation.jsx
@@ -1,20 +1,14 @@
 import { cn } from "../../utils";
 import Tab from "../Tab";
 import { Button, Div } from "../base";
-
-const tabs = [
-  {id: 0, title: "Home"},
-  {id: 1, title: "Category"},
-  {id: 2, title: "Blog"},
-  {id: 5, title: "About Us"}
-]
+import { navTabs } from "../Data";
 
 const Navigation = ({className, ...rest}) => {
   return (
     <Div className={cn('flex justify-between', className)} {...rest}>
       <Div>Logo</Div>
       <Div className={cn('flex justify-between gap-10')}>
-        {tabs.map(tab => {
+        {navTabs.map(tab => {
           return (
             <Tab key={tab.id} data={tab}/>
           )
