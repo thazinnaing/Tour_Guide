ea9b93b4e894f674be7ace3f2d46033314072380
Thazin <thazinnaingmm@gmail.com>
2024-01-09 18:14:45 +0630

    style: style input

--- src/components/base/Input.jsx
+++ src/components/base/Input.jsx
@@ -2,7 +2,7 @@ import { cn } from "../../utils"
 
 export const Input = ({className, text, ...rest}) => {
   return (
-    <input className={cn('px-2 bg-white text-black dark:bg-black dark:text-white outline-none sm:text-l', className)} type={text} {...rest}/>
+    <input className={cn('px-2 bg-white text-black outline-none sm:text-l', className)} type={text} {...rest}/>
   )
 }
 
