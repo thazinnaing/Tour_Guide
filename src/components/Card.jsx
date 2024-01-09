5f9882e0906d38c8a607019832eaac081e622069
Thazin <thazinnaingmm@gmail.com>
2024-01-09 16:31:36 +0630

    style: styling card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({card}) => {
   return (
     <Div className={cn('shadow-custom w-auto rounded-md overflow-hidden flex flex-col')}>
       <Div className={cn('flex-1')}>
-        <Img className={cn('h-64 w-full sm:w-max-full object-fill')} src={card.image} alt="" />
+        <Img className={cn('h-64 w-full sm:w-max-full rounded-ss-md object-fill')} src={card.image} alt="" />
       </Div>
       <Div className={cn("px-2 py-3")}>
         <Div className={cn('flex flex-col gap-1')}>
--- src/components/base/Img.jsx
+++ src/components/base/Img.jsx
@@ -2,7 +2,7 @@ import { cn } from "../../utils"
 
 export const Img = ({className, src, ...rest}) => {
   return (
-    <img className={cn('rounded-md',className)} src={src} {...rest}/>
+    <img className={cn('',className)} src={src} {...rest}/>
   )
 }
 
