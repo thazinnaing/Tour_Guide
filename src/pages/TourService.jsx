9d30f78bb97aa847ea9a941a3aa3202a1d0e593b
Thazin <thazinnaingmm@gmail.com>
2024-01-09 13:43:54 +0630

    feat: add image to TourService

--- src/pages/TourService.jsx
+++ src/pages/TourService.jsx
@@ -1,24 +1,23 @@
 import ServiceLogo from "../components/ServiceLogo"
-import { Div, H2, P } from "../components/base"
+import { Div, H2, H3, Img, P } from "../components/base"
 import { cn } from "../utils"
 import { logoData } from "../components/Data"
 
 
 const TourService = () => {  
   return (
-    <Div className={cn('w-full grid grid-cols-1 sm:grid-cols-2 gap-10')}>
-      <Div className={cn('h-72 sm:h-auto bg-gray-700')}>
-          
+    <Div className={cn('w-full sm:h-96 grid grid-cols-1 sm:grid-cols-2 gap-5 sm:gap-0')}>
+      <Div className={cn('')}>
+        <Img className={cn('w-full h-72 sm:h-96 sm:max-w-full')} 
+          src="https://img.freepik.com/free-photo/tourist-with-thumb-up_1368-7035.jpg?size=626&ext=jpg&ga=GA1.2.1633012741.1684645608&semt=ais" 
+          alt="" />
       </Div>
       <Div className={cn('flex flex-col gap-4')}>
         <Div>
-          <H2>Explore all corners of</H2>
-          <H2>The world with us</H2>
+          <H2>Explore all corners of The world with us</H2>
         </Div>
         <Div>
-          <P>Travel is fatal to prejudice, and narrow mindedness.</P>
-          <P>And many of our people need it sorely on these accounts.</P>
-          <P>And many of our people need sorely.</P>
+          <P>Travel is fatal to prejudice, and narrow mindedness. <br/> And many of our people need it sorely on these accounts.<br/> And many of our people need sorely.</P>
         </Div>
         <Div className={cn('flex')}>
           <ServiceLogo logoData={logoData[0]}/>
