8bb3cb2fe9d29f5432a631b91a2b811079527d6b
Thazin <thazinnaingmm@gmail.com>
2024-01-09 09:34:05 +0630

    fix: fix input section

--- src/components/base/Input.jsx
+++ src/components/base/Input.jsx
@@ -1,8 +1,8 @@
 import { cn } from "../../utils"
 
-export const Input = ({className, ...rest}) => {
+export const Input = ({className, text, ...rest}) => {
   return (
-    <input className={cn('px-2 outline-none sm:text-l', className)} type="text" {...rest}/>
+    <input className={cn('px-2 outline-none sm:text-l', className)} type={text} {...rest}/>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,7 +7,7 @@ const Signup = () => {
       <H2>Sign up to our newsletter</H2>
       <Div className={cn('flex h-16 w-2/3 rounded-md shadow-custom')}>
         <Div className={cn('flex w-4/6 sm:w-5/6 justify-center items-center')}>
-          <Input className={cn('w-full')} placeholder="Enter your email here" />
+          <Input className={cn('w-full')} type="text" placeholder="Enter your email here" />
         </Div>
         <Div className={cn('bg-primary w-2/6 sm:w-1/6 px-5 flex justify-center items-center text-white rounded-r-md')}>
           Submit
