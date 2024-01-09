da59c04f1979403da938d817ea8d66f97dd26441
Thazin <thazinnaingmm@gmail.com>
2024-01-09 12:44:33 +0630

    feat: add place images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,15 @@
 import { cn } from "../utils";
-import { Div, H3, P } from "./base";
+import { Div, H3, Img, P } from "./base";
 
 const Card = ({card}) => {
   return (
-    <Div className={cn('shadow-custom w-auto h-64 rounded-md overflow-hidden flex flex-col')}>
-      <Div className={cn("bg-primary flex-1  rounded-md")}/>
-      <Div className={cn("p-2")}>
+    <Div className={cn('shadow-custom w-auto rounded-md overflow-hidden flex flex-col')}>
+      <Div className={cn('flex-1')}>
+        <Img className={cn('h-64 w-full sm:w-max-full object-fill')} src={card.image} alt="" />
+      </Div>
+      <Div className={cn("px-2 py-3")}>
         <Div className={cn('flex flex-col gap-1')}>
           <H3>{card.title}</H3>
-        
           <P>{card.description}</P>
         </Div>
       </Div>
