dd8a14361b9289fdedc63fe17c9de80e3724a256
Thazin <thazinnaingmm@gmail.com>
2024-01-16 17:34:04 +0630

    feat: add detail more to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,14 +8,15 @@ const Card = ({card}) => {
     navigate(`/detail/${card.id}`);
   }
   return (
-    <Div onClick={onClickHandler} className={cn('shadow-custom sm:shadow-none w-auto rounded-md overflow-hidden flex flex-col transition duration-1 ease-in-out transform hover:-translate-y-1 md:hover:-translate-y-1.5 hover:shadow-custom')}>
+    <Div onClick={onClickHandler} className={cn('w-auto shadow-sm rounded-md overflow-hidden flex flex-col transition duration-1 ease-in-out transform hover:-translate-y-1 md:hover:-translate-y-1.5 hover:shadow-custom')}>
       <Div className={cn('flex-1')}>
         <Img className={cn('h-64 w-full sm:w-max-full rounded-ss-md object-fill')} src={card.image} alt="" />
       </Div>
-      <Div className={cn("px-2 py-3")}>
+      <Div className={cn("px-2 py-4")}>
         <Div className={cn('flex flex-col gap-1')}>
           <H3 className={cn('text-sm xs:text-base')}>{card.title}</H3>
           <P className={cn('text-sm xs:text-base')}>{card.description}</P>
+          <P className={cn('text-primary')}>Click for more detail ...</P>
         </Div>
       </Div>
     </Div>
